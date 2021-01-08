c503f348108afd7625274061df8fa840cd948b88
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2021-01-08 14:02:39 +0530

    e for exponential

--- thosesigns.go
+++ thosesigns.go
@@ -3,8 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Numberz: %b ", 2703) // %b is for binary
-	fmt.Printf("Numberz: %o ", 2703) // %o is for octal
-	fmt.Printf("Numberz: %d ", 2703) // %d is for decimal
-	fmt.Printf("Numberz: %x ", 2703) // %x is for hexa
+	fmt.Printf("Number: %e", 2.343332776767) //usual exponential stuff
 }
