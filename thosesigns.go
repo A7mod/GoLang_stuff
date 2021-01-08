f6093358c0add4ad841d9d63aa9b2ac8cf056643
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2021-01-08 13:59:13 +0530

    numbers binary to hexa

--- thosesigns.go
+++ thosesigns.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello %t", true) // %t is used for boolean (true/false)
-
+	fmt.Printf("Numberz: %b ", 2703) // %b is for binary
+	fmt.Printf("Numberz: %o ", 2703) // %o is for octal
+	fmt.Printf("Numberz: %d ", 2703) // %d is for decimal
+	fmt.Printf("Numberz: %x ", 2703) // %x is for hexa
 }
