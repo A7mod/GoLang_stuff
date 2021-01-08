fc462052bde105e08651e36acbca80dc50906d55
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2021-01-08 13:50:43 +0530

    %v for values

--- thosesigns.go
+++ thosesigns.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%T", 10) // %T shows us the type of data (here int)
+	fmt.Printf("Hello %T, %v", 10, 20) // %v shows us the value of data (here 20)
+
 }
