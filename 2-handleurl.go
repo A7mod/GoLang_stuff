cb275d68cd4ec7c90fd8cf46ebb982647721eac5
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-09-15 20:17:55 +0530

    errors, port is not visible

--- 2-handleurl.go
+++ 2-handleurl.go
@@ -17,10 +17,10 @@ func main() {
 	result, _ := url.Parse(myurl)
 
 	fmt.Println(result.Scheme)
-	// fmt.Println(result.Host)
-	// fmt.Println(result.Path)
-	// fmt.Println(result.Port())
-	// fmt.Println(result.RawQuery)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
+	//fmt.Println(result.RawQuery)
 
 	// qparams := result.Query()
 	// fmt.Printf("type? %T \n", qparams)
