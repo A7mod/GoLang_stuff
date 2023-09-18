aec7a01735fb9bba33441ae433a87737d5680034
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-09-18 23:11:44 +0530

    json data

--- 19-morejson.go
+++ 19-morejson.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type course struct {
 	Name     string
@@ -12,4 +15,23 @@ type course struct {
 
 func main() {
 	fmt.Println("JSON items")
+}
+
+func EncodeJson() {
+	devCourses := []course{
+		{"ReactJS Bootcamp", 299, "Learnonline.in", "abc123", []string{"web-dev", "js"}},
+		{"Python Bootcamp", 399, "Learnonline.in", "abc223", []string{"full-stack", "js"}},
+		{"Node Bootcamp", 1299, "Learnonline.in", "abc323", []string{"google", "js"}},
+		{"CP Bootcamp", 1199, "Learnonline.in", "abc423", nil},
+	}
+
+	// package this data as JSON data
+
+	finalJson, err := json.Marshal(devCourses)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s \n", finalJson)
+
 }
