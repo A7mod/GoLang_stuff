4a955ab9d3d3967f5efc9fce3443c5cfeb154bf9
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-09-18 17:25:03 +0530

    for this node thing, a go file

--- webserver/main.go
+++ webserver/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Welcome to web server : ")
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:8000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code :", response.StatusCode)
+	fmt.Println("COntent length :", response.ContentLength)
+
+	content, _ := iotuil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+
 }
