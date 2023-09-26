72a00965b5ccc692320d22dd2cfe4053fc214ad7
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-09-26 19:41:15 +0530

    settles methods in controller

--- 22-mongoapi/controller/controller.go
+++ 22-mongoapi/controller/controller.go
@@ -2,11 +2,18 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/a7mod/mongoapi/model"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
 )
 
 const connectionString = "mongodb+srv://general:a7mod@atlascluster.lmdzbps.mongodb.net/?retryWrites=true&w=majority"
@@ -37,5 +44,135 @@ func init() { // runs only in the beginning, only one time
 	// collection instance / reference
 
 	fmt.Println("Collection Instance is ready")
+
+}
+
+// mongoDB helpers - usually a file
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie with DB with id: ", inserted.InsertedID)
+}
+
+// update 1 record
+func updateOneMovie(movieID string) {
+	id, _ := primitive.ObjectIDFromHex(movieID)
+	filter := bson.M{"_id": id}                       //mongoDB understand BSON, it's like JSON but better.
+	update := bson.M{"$set": bson.M{"watched": true}} // bson.M vs bson.D is a debate
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
+
+}
+
+// delete 1 record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
+
+}
+
+// delete all records from mongoDB
+
+func deleteAllMovie() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of Movies Deleted: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+
+}
+
+// get all movies from mongoDB (database)
+func getAllMovies() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cur.Next(context.Background()) {
+		var movie bson.M
+		err := cur.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+
+	defer cur.Close(context.Background())
+	return movies
+
+}
+
+// actual controllers - what actually goes in the file
+
+func GetMyAllMovies(w http.ResponseWriter, r *http.Response) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencode")
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Response) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+	var movie model.Netflix
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+
+// marking movie as watched...
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Response) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
+
+	params := mux.Vars(r)
+	updateOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+
+}
+
+// delete a movie
+
+func DeleteOneMovie(w http.ResponseWriter, r *http.Response) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	params := mux.Vars(r)
+	deleteOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+
+}
+
+func DeleteAllMovies(w http.ResponseWriter, r *http.Response) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	count := deleteAllMovie()
+	json.NewEncoder(w).Encode(count)
 
 }
