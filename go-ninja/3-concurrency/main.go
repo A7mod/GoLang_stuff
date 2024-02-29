81c39ea4f1da4976baa53781383f4fccd2720b31
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2024-02-29 20:29:28 +0530

    buffered channels understood

--- go-ninja/3-concurrency/main.go
+++ go-ninja/3-concurrency/main.go
@@ -5,14 +5,16 @@ package main
 import "fmt"
 
 func main() {
-	channel := make(chan string, 1) // step 3 : adding capacity to the channel, makes it a buffered channel, and we no longer experience deadlock
-
+	channel := make(chan string, 2) // step 3 : adding capacity to the channel, makes it a buffered channel, and we no longer experience deadlock
+	// to make step 4 work, we'll need to increase capacity here , then it will work fine
 	// trying to understand if this works this way. This is step 2. It will fail (deadlock)
 	channel <- "First Message"
+	channel <- "Second Message" // step 4: trying to shove channel with another message, resulting in deadlock
 	// go func() {
 	// 	channel <- "First Message"
 	// }()
 	fmt.Println(<-channel) // Prints ---- First Message
+	fmt.Println(<-channel) // Prints ---- Second Message
 
 	//initially the main go routine is running, then the other go routine starts when
 	// we use the go func(). (The initial go routine is continuing while all this is happening
