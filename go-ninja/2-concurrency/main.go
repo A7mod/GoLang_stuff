2672ece568ae3a55b290107ad2ca69ae5ffa6cf6
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2024-02-29 18:50:02 +0530

    making and understanding channel(s)

--- go-ninja/2-concurrency/main.go
+++ go-ninja/2-concurrency/main.go
@@ -11,12 +11,15 @@ func main() {
 		fmt.Println(time.Since(now))
 	}() //this is a function call, that's how this defer func works
 
+	smokeSignal := make(chan bool) // this is a boolean channel (true or false for when we're done)
 	evilNinja := "Tommy"
-	go attack(evilNinja)
+	go attack(evilNinja, smokeSignal) // passed as argument? Not the right terminology
 	//time.Sleep(time.Second * 2) // this is making it delay more so, we gon remove this
+	fmt.Println(<-smokeSignal)
 }
 
-func attack(target string) {
+func attack(target string, attacked chan bool) { // here as well, needs to be understood
 	time.Sleep(time.Second)
 	fmt.Println("Throwing ninja stars at", target)
+	attacked <- true // hey the attack is done here
 }
