599df4ad455b7c1c5802a2e4552ce9a3e2657c60
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2024-02-29 18:51:45 +0530

    messages

--- go-ninja/2-concurrency/main.go
+++ go-ninja/2-concurrency/main.go
@@ -15,11 +15,11 @@ func main() {
 	evilNinja := "Tommy"
 	go attack(evilNinja, smokeSignal) // passed as argument? Not the right terminology
 	//time.Sleep(time.Second * 2) // this is making it delay more so, we gon remove this
-	fmt.Println(<-smokeSignal)
+	fmt.Println(<-smokeSignal) // receiving the message
 }
 
 func attack(target string, attacked chan bool) { // here as well, needs to be understood
 	time.Sleep(time.Second)
 	fmt.Println("Throwing ninja stars at", target)
-	attacked <- true // hey the attack is done here
+	attacked <- true // hey the attack is done here || sendong the message
 }
